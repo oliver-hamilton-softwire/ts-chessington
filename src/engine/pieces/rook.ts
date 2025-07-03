6e2dff83752bb62f7eecc098747fc420da9e2433
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 09:58:18 +0100

    Allows lateral moves, and no other moves, for rooks

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,25 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        // Allow lateral moves, and no other moves
+        return [
+            // Horizontal
+            Square.at(currentSquare.row, 0),
+            Square.at(currentSquare.row, 1),
+            Square.at(currentSquare.row, 3),
+            Square.at(currentSquare.row, 4),
+            Square.at(currentSquare.row, 5),
+            Square.at(currentSquare.row, 6),
+            Square.at(currentSquare.row, 7),
+            // Vertical
+            Square.at(0, currentSquare.col),
+            Square.at(2, currentSquare.col),
+            Square.at(3, currentSquare.col),
+            Square.at(4, currentSquare.col),
+            Square.at(5, currentSquare.col),
+            Square.at(6, currentSquare.col),
+            Square.at(7, currentSquare.col)
+        ];
     }
 }
