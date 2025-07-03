c48720b35623e4a2fd2ee6e21860e5f3bfc048b1
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 11:59:17 +0100

    Queen cannot move through friendly / opposing pieces

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -12,33 +12,40 @@ export default class Queen extends Piece {
         const currentSquare = board.findPiece(this);
         // Allow lateral and diagonal moves, and no other moves
         const moves = [];
-        for (let i = 0; i <= 7; i++) {
-            if (!currentSquare.equals(Square.at(currentSquare.row, i))) {
-                moves.push(Square.at(currentSquare.row, i));
-            }
-            if (!currentSquare.equals(Square.at(i, currentSquare.col))) {
-                moves.push(Square.at(i, currentSquare.col));
-            }
-           
+         // Squares below
+        for (let i = currentSquare.row - 1; i >= 0 && board.getPiece(Square.at(i, currentSquare.col)) == undefined; i--) {
+            moves.push(Square.at(i, currentSquare.col));
+        }
+        // Squares to the right
+        for (let j = currentSquare.col + 1; j <= 7 && board.getPiece(Square.at(currentSquare.row, j)) == undefined; j++) {
+            moves.push(Square.at(currentSquare.row, j));
+        }
+        // Squares above
+        for (let i = currentSquare.row + 1; i <= 7 && board.getPiece(Square.at(i, currentSquare.col)) == undefined; i++) {
+            moves.push(Square.at(i, currentSquare.col));
+        }
+        // Squares to the left
+        for (let j = currentSquare.col - 1; j >= 0 && board.getPiece(Square.at(currentSquare.row, j)) == undefined; j--) {
+            moves.push(Square.at(currentSquare.row, j));
         }
 
         // Bottom-right of diagonal
-        for (let i = 1; i <= currentSquare.row && i <= 7 - currentSquare.col; i++) {
+        for (let i = 1; i <= currentSquare.row && i <= 7 - currentSquare.col && board.getPiece(Square.at(currentSquare.row - i, currentSquare.col + i)) == undefined; i++) {
             moves.push(Square.at(currentSquare.row - i, currentSquare.col + i));
         }
 
         // Bottom-left of diagonal
-        for (let i = 1; i <= currentSquare.row && i <= currentSquare.col; i++) {
+        for (let i = 1; i <= currentSquare.row && i <= currentSquare.col && board.getPiece(Square.at(currentSquare.row - i, currentSquare.col - i)) == undefined; i++) {
             moves.push(Square.at(currentSquare.row - i, currentSquare.col - i));
         }
 
         // Top-right of diagonal
-        for (let i = 1; i <= 7 - currentSquare.row && i <= 7 - currentSquare.col; i++) {
+        for (let i = 1; i <= 7 - currentSquare.row && i <= 7 - currentSquare.col && board.getPiece(Square.at(currentSquare.row + i, currentSquare.col + i)) == undefined; i++) {
             moves.push(Square.at(currentSquare.row + i, currentSquare.col + i));
         }
 
         // Top-left of diagonal
-        for (let i = 1; i <= 7 - currentSquare.row && i <= currentSquare.col; i++) {
+        for (let i = 1; i <= 7 - currentSquare.row && i <= currentSquare.col && board.getPiece(Square.at(currentSquare.row + i, currentSquare.col - i)) == undefined; i++) {
             moves.push(Square.at(currentSquare.row + i, currentSquare.col - i));
         }
 
