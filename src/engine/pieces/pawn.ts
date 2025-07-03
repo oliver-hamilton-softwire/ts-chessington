a505de848fd24ac3abe0b762ad7bda4acd49fe59
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 09:41:02 +0100

    Red-1 tests passing

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,13 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        if (this.player == Player.WHITE) {
+            return [Square.at(currentSquare.row + 1, currentSquare.col)];
+        }
+        // this.player == Player.BLACK
+        else {
+            return [Square.at(currentSquare.row - 1, currentSquare.col)];
+        }
     }
 }
