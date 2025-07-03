8d1e07d9b9e1e9da8313a43f7db09f9798c5b600
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 14:23:19 +0100

    Queen can take opposing pieces (except the king)

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import King from './king';
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -12,43 +13,24 @@ export default class Queen extends Piece {
         const currentSquare = board.findPiece(this);
         // Allow lateral and diagonal moves, and no other moves
         const moves = [];
-         // Squares below
-        for (let i = currentSquare.row - 1; i >= 0 && board.getPiece(Square.at(i, currentSquare.col)) == undefined; i--) {
-            moves.push(Square.at(i, currentSquare.col));
-        }
-        // Squares to the right
-        for (let j = currentSquare.col + 1; j <= 7 && board.getPiece(Square.at(currentSquare.row, j)) == undefined; j++) {
-            moves.push(Square.at(currentSquare.row, j));
-        }
-        // Squares above
-        for (let i = currentSquare.row + 1; i <= 7 && board.getPiece(Square.at(i, currentSquare.col)) == undefined; i++) {
-            moves.push(Square.at(i, currentSquare.col));
-        }
-        // Squares to the left
-        for (let j = currentSquare.col - 1; j >= 0 && board.getPiece(Square.at(currentSquare.row, j)) == undefined; j--) {
-            moves.push(Square.at(currentSquare.row, j));
-        }
 
         // Bottom-right of diagonal
-        for (let i = 1; i <= currentSquare.row && i <= 7 - currentSquare.col && board.getPiece(Square.at(currentSquare.row - i, currentSquare.col + i)) == undefined; i++) {
-            moves.push(Square.at(currentSquare.row - i, currentSquare.col + i));
-        }
-
+        moves.push(...board.getMovesInDirection(currentSquare, [-1, 1]));
         // Bottom-left of diagonal
-        for (let i = 1; i <= currentSquare.row && i <= currentSquare.col && board.getPiece(Square.at(currentSquare.row - i, currentSquare.col - i)) == undefined; i++) {
-            moves.push(Square.at(currentSquare.row - i, currentSquare.col - i));
-        }
-
+        moves.push(...board.getMovesInDirection(currentSquare, [-1, -1]));
         // Top-right of diagonal
-        for (let i = 1; i <= 7 - currentSquare.row && i <= 7 - currentSquare.col && board.getPiece(Square.at(currentSquare.row + i, currentSquare.col + i)) == undefined; i++) {
-            moves.push(Square.at(currentSquare.row + i, currentSquare.col + i));
-        }
-
+        moves.push(...board.getMovesInDirection(currentSquare, [1, 1]));
         // Top-left of diagonal
-        for (let i = 1; i <= 7 - currentSquare.row && i <= currentSquare.col && board.getPiece(Square.at(currentSquare.row + i, currentSquare.col - i)) == undefined; i++) {
-            moves.push(Square.at(currentSquare.row + i, currentSquare.col - i));
-        }
-
-        return moves;
+        moves.push(...board.getMovesInDirection(currentSquare, [1, -1]));
+        //  // Squares below
+        moves.push(...board.getMovesInDirection(currentSquare, [-1, 0]));
+        // // Squares to the right
+        moves.push(...board.getMovesInDirection(currentSquare, [0, 1]));
+        // // Squares above
+        moves.push(...board.getMovesInDirection(currentSquare, [1, 0]));
+        // // Squares to the left
+        moves.push(...board.getMovesInDirection(currentSquare, [0, -1]));
+        // Can't take the opposing king
+        return moves.filter(square => !(board.getPiece(square) instanceof King));
     }
 }
