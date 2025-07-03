0b52b7d2d41bcd1d25f05af8e8cc1b8bc66daab3
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 10:25:39 +0100

    Can make knight moves and jump over other pieces

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        // Allow a combination of moving one space in one direction and two spaces in the perpendicular direction
+        const moves = []
+        for (let i of [1, -1]) {
+            for (let j of [2, -2]) {
+                moves.push(Square.at(currentSquare.row + i, currentSquare.col + j));
+                moves.push(Square.at(currentSquare.row + j, currentSquare.col + i));
+            }
+        }
+        return moves;
     }
 }
