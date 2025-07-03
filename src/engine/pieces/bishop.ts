ed81cd991cda62ec8d4cddceb2fe5d91944ed52d
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 10:09:02 +0100

    Bishop can move diagonally, and cannot make any other moves

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,30 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        // Allow diagonal moves, and no other moves
+        const moves = [];
+
+        // Bottom-right of diagonal
+        for (let i = 1; i <= currentSquare.row && i <= 7 - currentSquare.col; i++) {
+            moves.push(Square.at(currentSquare.row - i, currentSquare.col + i));
+        }
+
+        // Bottom-left of diagonal
+        for (let i = 1; i <= currentSquare.row && i <= currentSquare.col; i++) {
+            moves.push(Square.at(currentSquare.row - i, currentSquare.col - i));
+        }
+
+        // Top-right of diagonal
+        for (let i = 1; i <= 7 - currentSquare.row && i <= 7 - currentSquare.col; i++) {
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col + i));
+        }
+
+        // Top-left of diagonal
+        for (let i = 1; i <= 7 - currentSquare.row && i <= currentSquare.col; i++) {
+            moves.push(Square.at(currentSquare.row + i, currentSquare.col - i));
+        }
+
+        return moves;
     }
 }
