d8c3008b8b3b02fe397e9716c71e68dd6fa96245
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 09:50:23 +0100

    Allows white pawns to move one or two squares on their first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -11,7 +11,16 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         const currentSquare = board.findPiece(this);
         if (this.player == Player.WHITE) {
-            return [Square.at(currentSquare.row + 1, currentSquare.col)];
+            // If the white pawn hasn't yet moved, then it can move one OR two spaces
+            if (currentSquare.row == 1) {
+                return [
+                    Square.at(currentSquare.row + 1, currentSquare.col),
+                    Square.at(currentSquare.row + 2, currentSquare.col)
+                ];
+            }
+            else {
+                return [Square.at(currentSquare.row + 1, currentSquare.col)];
+            }
         }
         // this.player == Player.BLACK
         else {
