6ed26530d38161064fe7561d24ef0b959594b9fe
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 10:29:59 +0100

    King can move to adjacent squares, and cannot move to any others

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,15 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const moves = [];
+        for (let i = -1; i <= 1; i++) {
+            for (let j = -1; j <= 1; j++) {
+                if (i != 0 || j != 0) {
+                    moves.push(Square.at(currentSquare.row + i, currentSquare.col + j));
+                }
+            }
+        }
+        return moves;
     }
 }
