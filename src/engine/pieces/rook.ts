1dcc71c6d4e0299e5b2992eb1596c0bfba204eac
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 14:56:16 +0100

    Refactoring Rook implementation

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -13,43 +13,14 @@ export default class Rook extends Piece {
         const currentSquare = board.findPiece(this);
         // Allow lateral moves, and no other moves
         const moves = [];
-        // Squares below
-
-        for (let i = currentSquare.row - 1; i >= 0 && board.getPiece(Square.at(i, currentSquare.col))?.player != board.currentPlayer; i--) {
-            moves.push(Square.at(i, currentSquare.col));
-
-            // If this piece is an opposing piece, then there are no more pieces to be considered in this direction
-            if (board.getPiece(Square.at(i, currentSquare.col)) != undefined) {
-                break;
-            }
+        
+        // Generate only direction vectors corresponding to lateral moves
+        // Namely, [1, 0], [-1, 0], [0, -1] and [0, 1]
+        for (let i of [1, -1]) {
+            moves.push(...board.getMovesInDirection(currentSquare, [i, 0]));
+            moves.push(...board.getMovesInDirection(currentSquare, [0, i]));
         }
-        // Squares to the right
-        for (let j = currentSquare.col + 1; j <= 7 && board.getPiece(Square.at(currentSquare.row, j))?.player != board.currentPlayer; j++) {
-            moves.push(Square.at(currentSquare.row, j));
 
-            // If this piece is an opposing piece, then there are no more pieces to be considered in this direction
-            if (board.getPiece(Square.at(currentSquare.row, j)) != undefined) {
-                break;
-            }
-        }
-        // Squares above
-        for (let i = currentSquare.row + 1; i <= 7 && board.getPiece(Square.at(i, currentSquare.col))?.player != board.currentPlayer; i++) {
-            moves.push(Square.at(i, currentSquare.col));
-
-            // If this piece is an opposing piece, then there are no more pieces to be considered in this direction
-            if (board.getPiece(Square.at(i, currentSquare.col)) != undefined) {
-                break;
-            }
-        }
-        // Squares to the left
-        for (let j = currentSquare.col - 1; j >= 0 && board.getPiece(Square.at(currentSquare.row, j))?.player != board.currentPlayer; j--) {
-            moves.push(Square.at(currentSquare.row, j));
-
-            // If this piece is an opposing piece, then there are no more pieces to be considered in this direction
-            if (board.getPiece(Square.at(currentSquare.row, j)) != undefined) {
-                break;
-            }
-        }
         // Can't take the opposing king
         return moves.filter(square => !(board.getPiece(square) instanceof King));
     }
