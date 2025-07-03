336c8165730b5056aa6edafa2cd6ea6cd74c894c
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-03 12:14:38 +0100

    Pawns cannot move at the edges of the board

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -2,6 +2,7 @@ import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
 import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -12,12 +13,12 @@ export default class Pawn extends Piece {
         const currentSquare = board.findPiece(this);
         const moves = [];
         const direction = this.player === Player.WHITE ? 1 : -1;
-        // Square ahead must be free (to move either 1 or 2 spaces)
-        if (board.getPiece(Square.at(currentSquare.row + direction, currentSquare.col)) == undefined) {
+        // Square ahead must be free (to move either 1 or 2 spaces), and within the bounds of the board
+        if (currentSquare.row + direction >= 0 && currentSquare.row + direction < GameSettings.BOARD_SIZE && board.getPiece(Square.at(currentSquare.row + direction, currentSquare.col)) == undefined) {
             moves.push(Square.at(currentSquare.row + direction, currentSquare.col));
             const startingRow = this.player === Player.WHITE ? 1 : 6;
-            // If the pawn hasn't yet moved, then it can move two spaces (if the square two spaces ahead is free)
-            if (currentSquare.row == startingRow && board.getPiece(Square.at(currentSquare.row + 2 * direction, currentSquare.col)) == undefined) {
+            // If the pawn hasn't yet moved, then it can move two spaces (if the square two spaces ahead is free and within the bounds)
+            if (currentSquare.row + 2 * direction >= 0 && currentSquare.row + 2 * direction < GameSettings.BOARD_SIZE && currentSquare.row == startingRow && board.getPiece(Square.at(currentSquare.row + 2 * direction, currentSquare.col)) == undefined) {
                 moves.push(Square.at(currentSquare.row + 2 * direction, currentSquare.col));
             }
         }
